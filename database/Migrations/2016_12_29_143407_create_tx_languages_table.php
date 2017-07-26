578d714db5ab2ce0c7dcf2df088d8140e127e3ac
Nasztanovics Dávid <dnasztanovics@gmail.com>
2017-07-26 13:02:34 +0200

    update migration for merge with cosima #1

--- database/Migrations/2016_12_29_143407_create_tx_languages_table.php
+++ database/Migrations/2016_12_29_143407_create_tx_languages_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLanguagesTable extends Migration
+class CreateTxLanguagesTable extends Migration
 {
 
     /**
